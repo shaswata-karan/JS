91619c694ab22a1c1769a3b211477363af51d591
Shaswata Karan <shaswatakaran2016@gmail.com>
2023-09-21 13:18:02 +0530

    update

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,19 @@
+const accountID = 9836971416
+let accountEmail = "shaswatakaran2016@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 2 // not allowed
+
+accountEmail = "abc@gmail.com"
+accountPassword = "54321"
+accountCity = "Kolkata"
+
+/*
+Prefer not to use var
+because of issue in block scope and function scope
+*/
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
